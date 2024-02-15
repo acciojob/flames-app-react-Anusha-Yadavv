bc8a61876bc865d90a9a6847518dad1db1772b99
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-02-15 14:11:24 +0530

    logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-import FlamesApp from 
+import FlamesApp from "./FlamesApp";
 
 class App extends Component {
     render() {
+        
 
         return (
             <div id="main">
